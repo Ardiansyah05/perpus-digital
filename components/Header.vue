18ddd6f5cc16b968d52066adb98d2ad08999ff2a
Rdhii <142205085+Ardiansyah05@users.noreply.github.com>
2024-05-16 14:50:09 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar bg-primary text-white">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="~/assets/img/logo smk.png" alt="Bootstrap" width="88">
+      </a>
+      <div class="tittle me-auto">
+        <h1>Perpus Digital</h1>
+          <address>
+            SMKN 4 Tasikmalaya<br>
+            Jl.Depok, Sukamenak, Purbaratu
+        </address>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.logo {
+  width: 100%;
+}
+</style>
---
+++ pages/rincian_buku/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container p-2 mt-3">
+    <div class="row mt-4">
+      <div class="col-3">
+        <img src="~/assets/img/Screenshot 2024-02-26 131043.jpg" class="cover" alt="cover 1" width="270px">
+      </div>
+
+      <div class="col-4">
+        <h2>Rincian Buku</h2>
+        <p>Judul buku : 3 Babi kecil dan serigala jahat</p>
+        <p>Penulis : Annisa Fianni Sisma</p>
+        <p>Tahun terbit : 10 November 2023, 10:05</p>
+        <p>Halaman : 20 halaman</p>
+        <p>Kategori : Dongeng</p>
+        <p>ISBN : 059-828-3948-39-9</p>
+      </div>
+    </div>
+    <div class="row">
+      <p>Dahulu kala, di tengah hutan, hiduplah sebuah keluarga Babi yang sangat bahagia, terdiri dari ayah, ibu, dan tiga
+        anak Babi. Suatu hari, sang ibu dengan sengaja memanggil anak-anaknya untuk berkumpul. Ibu itu dengan penuh kasih
+        sayang memberi tugas kepada ketiga anak Babi tersebut agar mulai hidup mandiri. Ibu mereka memberikan perintah
+        untuk belajar membangun rumah masing-masing dan mencari bahan bangunan sendiri.</p>
+    </div>
+    <nuxt-link to="/buku/">
+      <button type="button" class="btn btn-secondary" disabled>KEMBALI</button>
+    </nuxt-link>
+  </div>
+</template>
