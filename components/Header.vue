7aebaa6aea2dfe6553edc9ecdd69ddb766251313
Rdhii <142205085+Ardiansyah05@users.noreply.github.com>
2024-07-22 11:05:25 +0700

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <nav class="navbar bg-primary text-white">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-        <img src="/_nuxt/logo smk.Ba-Cc_BE.png" alt="Bootstrap" width="88" height="88" data-v-641e567a="">
+        <img src="/_nuxt/logo smk.Ba-Cc_BE.webp" alt="Bootstrap" width="88" height="88" data-v-641e567a="">
       </a>
       <div class="tittle me-auto">
         <h1>Perpus Digital</h1>
